319cc5113ca71f6191f9901533eb56b37ddf9217
康勇 <kan_yon@foxmail.com>
2020-10-26 14:51:03 +0800

    1

--- main.go
+++ main.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
-	fmt.Println("za wa ru do!")
+	var emoji rune = 128515
+	fmt.Printf("%c\n", emoji)
+	emoje2 := "😃"
+	fmt.Println(emoje2)
+	i := 65
+	fmt.Printf("%c", i)
 }
