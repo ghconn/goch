d895b89f9f2bff561e802e176d4ba24b7d1a1424
康勇 <kan_yon@foxmail.com>
2020-10-26 16:20:41 +0800

    1

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("za wa ru do!")
+}
+
+var arr list.List
+
+func loopbody(m int32, n int32, nth int32) int32 {
+	return 0
+}
+
+func loopbodyofrationalnumber(m int32, n int32) int32 {
+	//如果整除
+	if m == 0 || n == 0 {
+
+	}
+	return 0
 }
