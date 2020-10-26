9da8cfed3ac585051d8c415c84b3f8ddc1924f34
康勇 <kan_yon@foxmail.com>
2020-10-26 11:20:58 +0800

    init

--- main.go
+++ main.go
@@ -2,24 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	num := 996
-	min := 0
-	max := 1001
-	n := rand.Intn(max-min-1) + min + 1
-	for num != n {
-		fmt.Printf("数字为：%2v\n", n)
-		if n > num {
-			max = n
-		} else {
-			min = n
-		}
-		n = rand.Intn(max-min-1) + min + 1
-		time.Sleep(time.Second)
-	}
-	fmt.Println("greet!")
+	fmt.Println("za wa ru do!")
 }
