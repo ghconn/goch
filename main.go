ddfb3ea7c89f561d97a652138078e2961f90a7e7
康勇 <kan_yon@foxmail.com>
2020-10-26 14:33:34 +0800

    1

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 )
 
 func main() {
-	fmt.Println("za wa ru do!")
+	distance := new(big.Int)
+	distance.SetString("240000000000000000000", 10)
+	fmt.Println(distance)
+
+	lightSpeed := big.NewInt(200792)
+	secondsPerDay := big.NewInt(86400)
+
+	seconds := new(big.Int)
+	seconds.Div(distance, lightSpeed)
+
+	days := new(big.Int)
+	days.Div(seconds, secondsPerDay)
+
+	fmt.Println("That is", days, "days of travel at light speeds.")
 }
