002dd7603b4e08531a77790e4d8e4649d2b5d0e3
康勇 <kan_yon@foxmail.com>
2020-11-11 16:05:58 +0800

    complete

--- main.go
+++ main.go
@@ -6,43 +6,45 @@ import (
 )
 
 func main() {
-	m := 22
-	n := 122
-	nth := 100
-	compute(m, n, nth, func(mixture *list.List, index int, lpbody *list.List, re int8) {
+	m := 229
+	n := 1222
+	nth := 2000
+	compute(m, n, nth, func(mixture *list.List, lpbody *list.List, re int8) {
 		if mixture.Len() > 0 {
-			fmt.Println("mixture:", mixture, "从第", index, "位出现循环体")
+			fmt.Print("mixture:")
+			loopthroughprint(mixture)
+			fmt.Printf("从第%v位出现循环体", index+1)
 		} else {
 			fmt.Println("从第1位出现循环体")
 		}
 		fmt.Println()
-		fmt.Printf("小数部分第%v位的数字:%v\n", n, re)
+		fmt.Printf("小数部分第%v位的数字:%v\n", nth, re)
 		fmt.Println()
 		fmt.Println("循环体：")
-		fmt.Println(lpbody)
+		loopthroughprint(lpbody)
+		fmt.Println("循环体长度：", lpbody.Len())
 	})
 }
 
-type cpcb func(*list.List, int, *list.List, int8)
+type cpcb func(*list.List, *list.List, int8)
 
-var arr list.List
+var index int
 
 func compute(m int, n int, nth int, action cpcb) { // nth 要求的第几位
 	mixture := list.New() // mixture 计算出的混合体
-	var index int
 	var re int8
 	lpbody := list.New()
 
 	//m 被除数 n 除数 index 计算出的第几位出现循环体 lpbody 计算出的循环体
-	loopbody(m, n, index, lpbody)
+	loopbody(m, n, lpbody)
 
 	if index > 0 {
-		mixture = slice(lpbody, 0, index)
+		slicetoanother(lpbody, mixture, 0, index)
 	}
 
 	nth = nth - 1
 	if nth < index {
-		re = elementAt(lpbody, nth).(int8)
+		re = int8(elementAt(lpbody, nth).(int))
 		if index == lpbody.Len() {
 			lpbody = list.New()
 			lpbody.PushBack(0)
@@ -55,46 +57,42 @@ func compute(m int, n int, nth int, action cpcb) { // nth 要求的第几位
 			lpbody.PushBack(0)
 		} else {
 			splice(lpbody, 0, index)
-			re = int8(indexOf(lpbody, (nth-index)%lpbody.Len()))
+			re = int8(elementAt(lpbody, (nth-index)%lpbody.Len()).(int))
 		}
 	}
 
-	action(mixture, index, lpbody, re)
+	action(mixture, lpbody, re)
 }
 
-func loopbody(m int, n int, index int, lpbody *list.List) {
+func loopbody(m int, n int, lpbody *list.List) {
 	if m == 0 || m == n {
-		mapmod10(lpbody)
+		mapdiv(lpbody, n)
 		index = lpbody.Len()
 	} else if m > n {
 		m = m % n
-		loopbody(m, n, index, lpbody)
+		loopbody(m, n, lpbody)
 	} else {
-		if indexOf(lpbody, m*10) == -1 {
+		index = indexOf(lpbody, m*10)
+		if index == -1 {
 			lpbody.PushBack(m * 10)
 			m = m * 10 % n
-			loopbody(m, n, index, lpbody)
+			loopbody(m, n, lpbody)
 		} else {
-			index = indexOf(lpbody, m*10)
 			mapdiv(lpbody, n)
 		}
 	}
 }
 
 func mapmod10(intlist *list.List) {
-	temp := list.New()
 	for x := intlist.Front(); x != nil; x = x.Next() {
-		temp.PushBack(x.Value.(int) % 10)
+		x.Value = x.Value.(int) % 10
 	}
-	intlist = temp
 }
 
 func mapdiv(intlist *list.List, divisor int) {
-	temp := list.New()
 	for x := intlist.Front(); x != nil; x = x.Next() {
-		temp.PushBack(x.Value.(int) / divisor)
+		x.Value = x.Value.(int) / divisor
 	}
-	intlist = temp
 }
 
 func indexOf(lst *list.List, v interface{}) int {
@@ -115,22 +113,31 @@ func elementAt(lst *list.List, index int) interface{} {
 	return nil
 }
 
-func slice(lst *list.List, start int, end int) *list.List { // [start, end]
-	var temp *list.List
-	for i, x := 0, lst.Front(); x != nil && i <= end; i, x = i+1, x.Next() {
+func slicetoanother(lst *list.List, otherlst *list.List, start int, end int) { // [start, end)
+	for i, x := 0, lst.Front(); x != nil && i < end; i, x = i+1, x.Next() {
 		if i >= start {
-			temp.PushBack(x.Value)
+			otherlst.PushBack(x.Value)
 		}
 	}
-	return temp
 }
 
-func splice(lst *list.List, start int, end int) { // [start, end]
+func splice(lst *list.List, start int, end int) { // [start, end)
 	var tempelement *list.Element
-	for i, x := 0, lst.Front(); x != nil && i <= end; i, x = i+1, tempelement {
-		if i < start || i > end {
+	for i, x := 0, lst.Front(); x != nil && i < end; i, x = i+1, tempelement {
+		if i >= start {
 			tempelement = x.Next()
 			lst.Remove(x)
 		}
 	}
+}
+
+// 遍历打印
+func loopthroughprint(lst *list.List) {
+	for x := lst.Front(); x != nil; x = x.Next() {
+		fmt.Print(x.Value)
+		if x.Next() != nil {
+			fmt.Print(",")
+		}
+	}
+	fmt.Println()
 }
